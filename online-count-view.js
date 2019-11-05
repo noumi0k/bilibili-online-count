930de82fed44de8dc93bffa04a2fa78b1fbb5973
noumi0k <noumi0k@gmail.com>
2019-11-05 15:56:36 +0900

    ブロック無視した場合の実装をちゃんとした

--- online-count-view.js
+++ online-count-view.js
@@ -3,14 +3,8 @@ window.onload = function(){
 }
 
 function showOnlineCount(){
-    var mid = "452189" //チャンネルID
-    var json = getJson(mid);
-    console.log(json["data"]["online"]);
-}
-
-//ちゃんとブロックされててできなかった
-function getJson(mid){
-    fetch("https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid=" + mid
+    var mid = getChannelId()["id"]//チャンネルID
+    fetch("https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid="+mid
     ,{
         mode: 'no-cors',
         credentials: 'include'
@@ -22,5 +16,38 @@ function getJson(mid){
                 throw new Error(`Request failed: ${response.status}`);
             }
         })
+        .then(function(json){
+            console.log(json);
+            var onlineCountText = json["data"]["online"]
+            console.log(onlineCountText);
+            var htmlText = document.getElementById('online-count');
+            htmlText.innerHTML = onlineCountText;
+        })
         .catch(err => console.error(err));
+}
+
+//ここパクった
+//https://www.ipentec.com/document/javascript-get-parameter
+function getChannelId(){
+    if (1 < document.location.search.length) {
+        // 最初の1文字 (?記号) を除いた文字列を取得する
+         var query = document.location.search.substring(1);
+    
+        // クエリの区切り記号 (&) で文字列を配列に分割する
+         var parameters = query.split('&');
+    
+        var result = new Object();
+        for (var i = 0; i < parameters.length; i++) {
+          // パラメータ名とパラメータ値に分割する
+           var element = parameters[i].split('=');
+    
+          var paramName = decodeURIComponent(element[0]);
+          var paramValue = decodeURIComponent(element[1]);
+    
+          // パラメータ名をキーとして連想配列に追加する
+           result[paramName] = decodeURIComponent(paramValue);
+        }
+        return result;
+      }
+      return null;
 }
